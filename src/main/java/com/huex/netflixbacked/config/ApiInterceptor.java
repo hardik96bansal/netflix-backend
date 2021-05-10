83fdb9e336e057314c54bc7bcc4276661e40522c
Hardik Bansal <hardik.bansal@hashedin.com>
2021-05-10 19:23:47 +0530

    Added JUnit Test Case

---
+++ src/main/java/com/huex/netflixbacked/config/ApiInterceptor.java
@@ -0,0 +1,38 @@
+package com.huex.netflixbacked.config;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+public class ApiInterceptor extends HandlerInterceptorAdapter {
+
+    private static final String HEADER_TIME = "X_TIME_TO_EXECUTE";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String value = request.getHeader("X-Auth-Token");
+
+        if(value == null){
+            throw new Exception("Token Not Found");
+        }
+
+        String currentRequestId = UUID.randomUUID().toString();
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        request.setAttribute("requestId", currentRequestId);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executeTime = endTime - startTime;
+        response.addHeader(HEADER_TIME, String.valueOf(executeTime));
+        response.addHeader("X-Auth-Token", "token");
+    }
+}
--- src/test/java/com/huex/netflixbacked/services/MovieDetailsServiceTest.java
+++ src/test/java/com/huex/netflixbacked/services/MovieDetailsServiceTest.java
@@ -6,7 +6,6 @@ import com.huex.netflixbacked.repositories.MovieDetailsRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
